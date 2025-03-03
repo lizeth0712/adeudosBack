9d9d42023aaef072ed109a5d4445a5b53a4d336a
lizeth <sotolizeth50@gmail.com>
2025-03-02 23:57:58 -0700

    Habilitando CORS para permitir solicitudes desde el frontend

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const db = require("./db");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express .json());
 
 // Importar rutas
 const personaRoutes = require("./routes/personas");
